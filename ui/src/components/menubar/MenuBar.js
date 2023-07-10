60ed93ca6593bd4e688d5bfb8e0dd19e6fb7cf2a
Miles Morales <nazaritee@pm.me>
2023-07-10 15:31:42 -0500

    added menu bar icons light mode

--- ui/src/components/menubar/MenuBar.js
+++ ui/src/components/menubar/MenuBar.js
@@ -1,4 +1,13 @@
 import './MenuBar.css'
+import settingsIcon from '../menubar/icons/settings.png';
+import helpIcon from '../menubar/icons/help.png';
+import reminderIcon from '../menubar/icons/reminder.png';
+import goalIcon from '../menubar/icons/goal.png';
+import incomeIcon from '../menubar/icons/income.png';
+import expenseIcon from '../menubar/icons/expense.png';
+import transactionIcon from '../menubar/icons/transaction.png';
+import budgetIcon from '../menubar/icons/budget.png';
+
 
 function MenuBar() {
     return (
@@ -8,14 +17,15 @@ function MenuBar() {
             <div className="bottom_line"></div>
             <div className="hidden_text">Menu</div>
             <div className="hidden_menu">
-                <ul><a href="">Budget Overview</a></ul>
-                <ul><a href="">Transactions</a></ul>
-                <ul><a href="">Expenses</a></ul>
-                <ul><a href="">Income</a></ul>
-                <ul><a href="">Goals and Savings</a></ul>
-                <ul><a href="">Reminders and Alerts</a></ul>
-                <ul><a href="">Settings</a></ul>
-                <ul><a href="">Help and Support</a></ul>
+                {/* TODO: Add links for items */}
+                <ul><img src={budgetIcon}></img><a href="">Budget Overview</a></ul>
+                <ul><img src={transactionIcon}></img><a href="">Transactions</a></ul>
+                <ul><img src={expenseIcon}></img><a href="">Expenses</a></ul>
+                <ul><img src={incomeIcon}></img><a href="">Income</a></ul>
+                <ul><img src={goalIcon}></img><a href="">Goals and Savings</a></ul>
+                <ul><img src={reminderIcon}></img><a href="">Reminders and Alerts</a></ul>
+                <ul><img src={settingsIcon}></img><a href="">Settings</a></ul>
+                <ul><img src={helpIcon}></img><a href="">Help and Support</a></ul>
             </div>
         </div>
     );
