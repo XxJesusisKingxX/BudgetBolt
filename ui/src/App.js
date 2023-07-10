ff53bab87f11d18e43fb92562dc2c1a20f461989
Miles Morales <nazaritee@pm.me>
2023-07-10 08:29:08 -0500

    added react framework setup

---
+++ ui/src/App.js
@@ -0,0 +1,10 @@
+import './AppConfig.css';
+import Header from './components/header/Header'
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
---
+++ ui/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="title_container">
+            <div className="title">BUDGETBOLT</div>
+        </div>
+    );
+}
+  
+export default Header;
