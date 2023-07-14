435f3b2590c41631fc6f5a868d5b8b4593b8fb13
Miles Morales <nazaritee@pm.me>
2023-07-13 23:02:07 -0500

    added link integration with PLAID

---
+++ services/api/plaid/server.go
@@ -0,0 +1,346 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	plaid "github.com/plaid/plaid-go/v12/plaid"
+)
+
+var (
+	PLAID_CLIENT_ID                      = ""
+	PLAID_SECRET                         = ""
+	PLAID_ENV                            = ""
+	PLAID_PRODUCTS                       = ""
+	PLAID_COUNTRY_CODES                  = ""
+	PLAID_REDIRECT_URI                   = ""
+	APP_PORT                             = ""
+	client              *plaid.APIClient = nil
+)
+
+var environments = map[string]plaid.Environment{
+	"sandbox":     plaid.Sandbox,
+	"development": plaid.Development,
+	"production":  plaid.Production,
+}
+
+func init() {
+	// load env vars from .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error when loading environment variables from .env file %w", err)
+	}
+
+	// set constants from env
+	PLAID_CLIENT_ID = os.Getenv("PLAID_CLIENT_ID")
+	PLAID_SECRET = os.Getenv("PLAID_SECRET")
+
+	if PLAID_CLIENT_ID == "" || PLAID_SECRET == "" {
+		log.Fatal("Error: PLAID_SECRET or PLAID_CLIENT_ID is not set. Did you copy .env.example to .env and fill it out?")
+	}
+
+	PLAID_ENV = os.Getenv("PLAID_ENV")
+	PLAID_PRODUCTS = os.Getenv("PLAID_PRODUCTS")
+	PLAID_COUNTRY_CODES = os.Getenv("PLAID_COUNTRY_CODES")
+	PLAID_REDIRECT_URI = os.Getenv("PLAID_REDIRECT_URI")
+	APP_PORT = os.Getenv("APP_PORT")
+
+	// set defaults
+	if PLAID_PRODUCTS == "" {
+		PLAID_PRODUCTS = "transactions"
+	}
+	if PLAID_COUNTRY_CODES == "" {
+		PLAID_COUNTRY_CODES = "US"
+	}
+	if PLAID_ENV == "" {
+		PLAID_ENV = "sandbox"
+	}
+	if APP_PORT == "" {
+		APP_PORT = "8000"
+	}
+	if PLAID_CLIENT_ID == "" {
+		log.Fatal("PLAID_CLIENT_ID is not set. Make sure to fill out the .env file")
+	}
+	if PLAID_SECRET == "" {
+		log.Fatal("PLAID_SECRET is not set. Make sure to fill out the .env file")
+	}
+
+	// create Plaid client
+	configuration := plaid.NewConfiguration()
+	configuration.AddDefaultHeader("PLAID-CLIENT-ID", PLAID_CLIENT_ID)
+	configuration.AddDefaultHeader("PLAID-SECRET", PLAID_SECRET)
+	configuration.UseEnvironment(environments[PLAID_ENV])
+	client = plaid.NewAPIClient(configuration)
+}
+
+func main() {
+	r := gin.Default()
+
+	// Create a link token with the redirectURI (as white listed at https://dashboard.plaid.com/team/api).
+	// Once the flow succeeds, Plaid Link will redirect to redirectURI with
+	// additional parameters (as required by OAuth standards and Plaid).
+	r.POST("/api/create_link_token", createLinkToken)
+	// Re-initialize with the link token (from step 1) and the full received redirect URI
+	// from step 2.
+	r.POST("/api/info", info)
+	r.POST("/api/set_access_token", getAccessToken)
+	r.GET("/api/accounts", accounts)
+	r.GET("/api/balance", balance)
+	r.GET("/api/transactions", transactions)
+	r.GET("/api/investments_transactions", investmentTransactions)
+	r.GET("/api/holdings", holdings)
+
+	err := r.Run(":" + APP_PORT)
+	if err != nil {
+		panic("unable to start server")
+	}
+}
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store.
+var accessToken string
+var itemID string
+
+func renderError(c *gin.Context, originalErr error) {
+	if plaidError, err := plaid.ToPlaidError(originalErr); err == nil {
+		// Return 200 and allow the front end to render the error.
+		c.JSON(http.StatusOK, gin.H{"error": plaidError})
+		return
+	}
+
+	c.JSON(http.StatusInternalServerError, gin.H{"error": originalErr.Error()})
+}
+
+func getAccessToken(c *gin.Context) {
+	publicToken := c.PostForm("public_token")
+	ctx := context.Background()
+
+	// exchange the public_token for an access_token
+	exchangePublicTokenResp, _, err := client.PlaidApi.ItemPublicTokenExchange(ctx).ItemPublicTokenExchangeRequest(
+		*plaid.NewItemPublicTokenExchangeRequest(publicToken),
+	).Execute()
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+
+	accessToken = exchangePublicTokenResp.GetAccessToken()
+	itemID = exchangePublicTokenResp.GetItemId()
+
+	fmt.Println("public token: " + publicToken)
+	fmt.Println("access token: " + accessToken)
+	fmt.Println("item ID: " + itemID)
+
+	c.JSON(http.StatusOK, gin.H{
+		"access_token": accessToken,
+		"item_id":      itemID,
+	})
+}
+
+func accounts(c *gin.Context) {
+	ctx := context.Background()
+
+	accountsGetResp, _, err := client.PlaidApi.AccountsGet(ctx).AccountsGetRequest(
+		*plaid.NewAccountsGetRequest(accessToken),
+	).Execute()
+
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"accounts": accountsGetResp.GetAccounts(),
+	})
+
+	accountsGetResp.GetAccounts()
+	
+}
+
+func balance(c *gin.Context) {
+	ctx := context.Background()
+
+	balancesGetResp, _, err := client.PlaidApi.AccountsBalanceGet(ctx).AccountsBalanceGetRequest(
+		*plaid.NewAccountsBalanceGetRequest(accessToken),
+	).Execute()
+
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"accounts": balancesGetResp.GetAccounts(),
+	})
+}
+
+func transactions(c *gin.Context) {
+	ctx := context.Background()
+
+	// Set cursor to empty to receive all historical updates
+	var cursor *string
+
+	// New transaction updates since "cursor"
+	var added []plaid.Transaction
+	var modified []plaid.Transaction
+	var removed []plaid.RemovedTransaction // Removed transaction ids
+	hasMore := true
+	// Iterate through each page of new transaction updates for item
+	for hasMore {
+		request := plaid.NewTransactionsSyncRequest(accessToken)
+		if cursor != nil {
+			request.SetCursor(*cursor)
+		}
+		resp, _, err := client.PlaidApi.TransactionsSync(
+			ctx,
+		).TransactionsSyncRequest(*request).Execute()
+		if err != nil {
+			renderError(c, err)
+			return
+		}
+
+		// Add this page of results
+		added = append(added, resp.GetAdded()...)
+		modified = append(modified, resp.GetModified()...)
+		removed = append(removed, resp.GetRemoved()...)
+		hasMore = resp.GetHasMore()
+		// Update cursor to the next cursor
+		nextCursor := resp.GetNextCursor()
+		cursor = &nextCursor
+	}
+
+	sort.Slice(added, func(i, j int) bool {
+		return added[i].GetDate() < added[j].GetDate()
+	})
+	latestTransactions := added[len(added)-9:]
+
+	c.JSON(http.StatusOK, gin.H{
+		"latest_transactions": latestTransactions,
+	})
+}
+
+func investmentTransactions(c *gin.Context) {
+	ctx := context.Background()
+
+	endDate := time.Now().Local().Format("2006-01-02")
+	startDate := time.Now().Local().Add(-30 * 24 * time.Hour).Format("2006-01-02")
+
+	request := plaid.NewInvestmentsTransactionsGetRequest(accessToken, startDate, endDate)
+	invTxResp, _, err := client.PlaidApi.InvestmentsTransactionsGet(ctx).InvestmentsTransactionsGetRequest(*request).Execute()
+
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"investments_transactions": invTxResp,
+	})
+}
+
+func holdings(c *gin.Context) {
+	ctx := context.Background()
+
+	holdingsGetResp, _, err := client.PlaidApi.InvestmentsHoldingsGet(ctx).InvestmentsHoldingsGetRequest(
+		*plaid.NewInvestmentsHoldingsGetRequest(accessToken),
+	).Execute()
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"holdings": holdingsGetResp,
+	})
+}
+
+func info(context *gin.Context) {
+	context.JSON(http.StatusOK, map[string]interface{}{
+		"item_id":      itemID,
+		"access_token": accessToken,
+		"products":     strings.Split(PLAID_PRODUCTS, ","),
+	})
+}
+
+func createLinkToken(c *gin.Context) {
+	linkToken, err := linkTokenCreate(nil)
+	if err != nil {
+		renderError(c, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"link_token": linkToken})
+}
+
+func convertCountryCodes(countryCodeStrs []string) []plaid.CountryCode {
+	countryCodes := []plaid.CountryCode{}
+
+	for _, countryCodeStr := range countryCodeStrs {
+		countryCodes = append(countryCodes, plaid.CountryCode(countryCodeStr))
+	}
+
+	return countryCodes
+}
+
+func convertProducts(productStrs []string) []plaid.Products {
+	products := []plaid.Products{}
+
+	for _, productStr := range productStrs {
+		products = append(products, plaid.Products(productStr))
+	}
+
+	return products
+}
+
+// linkTokenCreate creates a link token using the specified parameters
+func linkTokenCreate(
+	paymentInitiation *plaid.LinkTokenCreateRequestPaymentInitiation,
+) (string, error) {
+	ctx := context.Background()
+
+	// Institutions from all listed countries will be shown.
+	countryCodes := convertCountryCodes(strings.Split(PLAID_COUNTRY_CODES, ","))
+	redirectURI := PLAID_REDIRECT_URI
+
+	// This should correspond to a unique id for the current user.
+	// Typically, this will be a user ID number from your application.
+	// Personally identifiable information, such as an email address or phone number, should not be used here.
+	user := plaid.LinkTokenCreateRequestUser{
+		ClientUserId: time.Now().String(),
+	}
+
+	request := plaid.NewLinkTokenCreateRequest(
+		"Plaid Quickstart",
+		"en",
+		countryCodes,
+		user,
+	)
+
+	if paymentInitiation != nil {
+		request.SetPaymentInitiation(*paymentInitiation)
+		// The 'payment_initiation' product has to be the only element in the 'products' list.
+		request.SetProducts([]plaid.Products{plaid.PRODUCTS_PAYMENT_INITIATION})
+	} else {
+		products := convertProducts(strings.Split(PLAID_PRODUCTS, ","))
+		request.SetProducts(products)
+	}
+
+	if redirectURI != "" {
+		request.SetRedirectUri(redirectURI)
+	}
+
+	linkTokenCreateResp, _, err := client.PlaidApi.LinkTokenCreate(ctx).LinkTokenCreateRequest(*request).Execute()
+
+	if err != nil {
+		return "", err
+	}
+
+	return linkTokenCreateResp.GetLinkToken(), nil
+}
---
+++ ui/src/App.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useContext, useCallback } from "react";
+
+import Link from "./Components/plaid/PlaidAccountConnect";
+import Context from "./Context";
+import Header from "./Components/header/Header";
+import Home from "./pages/home/Home";
+import MenuBar from "./Components/menubar/MenuBar";
+
+const App = () => {
+  const { linkSuccess, isItemAccess, isPaymentInitiation, dispatch } = useContext(Context);
+
+  const getInfo = useCallback(async () => {
+    const response = await fetch("/api/info", { method: "POST" });
+    if (!response.ok) {
+      dispatch({ type: "SET_STATE", state: { backend: false } });
+      return { paymentInitiation: false };
+    }
+    const data = await response.json();
+    const paymentInitiation: boolean = data.products.includes(
+      "payment_initiation"
+    );
+    dispatch({
+      type: "SET_STATE",
+      state: {
+        products: data.products,
+        isPaymentInitiation: paymentInitiation,
+      },
+    });
+    return { paymentInitiation };
+  }, [dispatch]);
+
+  const generateToken = useCallback(
+    async (isPaymentInitiation) => {
+      // Link tokens for 'payment_initiation' use a different creation flow in your backend.
+      const path = isPaymentInitiation
+        ? "/api/create_link_token_for_payment"
+        : "/api/create_link_token";
+      const response = await fetch(path, {
+        method: "POST",
+      });
+      if (!response.ok) {
+        dispatch({ type: "SET_STATE", state: { linkToken: null } });
+        return;
+      }
+      const data = await response.json();
+      if (data) {
+        if (data.error != null) {
+          dispatch({
+            type: "SET_STATE",
+            state: {
+              linkToken: null,
+              linkTokenError: data.error,
+            },
+          });
+          return;
+        }
+        dispatch({ type: "SET_STATE", state: { linkToken: data.link_token } });
+      }
+      // Save the link_token to be used later in the Oauth flow.
+      localStorage.setItem("link_token", data.link_token);
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    const init = async () => {
+      const { paymentInitiation } = await getInfo(); // used to determine which path to take when generating token
+      // do not generate a new token for OAuth redirect; instead
+      // setLinkToken from localStorage
+      if (window.location.href.includes("?oauth_state_id=")) {
+        dispatch({
+          type: "SET_STATE",
+          state: {
+            linkToken: localStorage.getItem("link_token"),
+          },
+        });
+        return;
+      }
+      generateToken(paymentInitiation);
+    };
+    init();
+  }, [dispatch, generateToken, getInfo]);
+
+  return (
+    <>
+      <Header>
+        <MenuBar/>
+        <Home/>
+        <Link/>
+      </Header>
+    </>
+  );
+};
+
+export default App;
