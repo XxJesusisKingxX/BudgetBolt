1e826d51cdc11d8685664895946672ed415c5309
Miles Morales <nazaritee@pm.me>
2023-07-24 01:42:24 -0500

    added recent transactions peek ability

---
+++ src/services/databases/postgresql/view/profile.go
@@ -0,0 +1,18 @@
+package view
+
+import (
+	"database/sql"
+	"strings"
+
+	"budgetbolt/src/services/databases/postgresql/model"
+)
+
+func ViewProfile(rows *sql.Rows) model.Profile {
+	var id int
+	var user string
+	var pass string
+	rows.Next() 
+	rows.Scan(&id, &user, &pass)
+	view := model.Profile{ID: id, Name: strings.TrimSpace(user), Password: strings.TrimSpace(pass)}
+	return view
+}
